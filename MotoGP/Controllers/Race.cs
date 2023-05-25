0c6886156ab81078238baf929bf92395ef1fa62b
LwamB <Lwamberihu0@gmail.com>
2023-05-25 10:41:16 +0200

    Add project files.

---
+++ MotoGP/Controllers/Race.cs
@@ -0,0 +1,13 @@
+﻿namespace MotoGP.Models
+{
+    public class Race
+    {
+        public int RaceID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public int Length { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ MotoGP/Controllers/Ticket.cs
@@ -0,0 +1,6 @@
+﻿namespace MotoGP.Models
+{
+    public class Ticket
+    {
+    }
+}
